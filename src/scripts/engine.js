e2814a9f5251c709311936d73fa9ea60aef66ee3
Mateus Rangel <mateusrangel.com.br@gmail.com>
2023-11-27 21:20:05 -0300

    implementando evento listener

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -7,7 +7,9 @@ const state = {
     },
     values: {
         timerId: null,
-        gameVelocity: 1000,
+        gameVelocity: 800,
+        hitPosition: 0,
+        result: 0,
     }
 }
 
@@ -19,21 +21,28 @@ function randomSquare() {
     let randomNumber = Math.floor(Math.random() * 9)
     let randomSquare = state.view.squares[randomNumber]
     randomSquare.classList.add('enemy')
+
+    state.values.hitPosition = randomSquare.id
 }
 
 function moveEnemy() {
-    state.values.timerID = setInterval(randomSquare, state.values.gameVelocity)
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
 }
 function addListenerHitBox() {
-    state.view.array.forEach(square => {
-        // if (square.id === ) {
-
-        // }
+    state.view.squares.forEach(square => {
+        square.addEventListener('click', () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.result++
+                state.view.score.textContent = state.values.result
+                state.values.hitPosition = null
+            }
+        })
     });
 }
 
 function initialize() {
     moveEnemy()
+    addListenerHitBox()
 }
 
 initialize()
